fb3d7577cb78315c00f0a819f972dd8591f14adb
Grazielly Souza <graziellysouza2580@gmail.com>
2024-04-22 19:47:02 -0300

    :tada: last commit?

---
+++ script.js
@@ -0,0 +1,13 @@
+function toggleMode() {
+    const html = document.documentElement
+
+    html.classList.toggle("light")
+}
+
+/* Abreviado...
+Const definindo uma variável que vai ser trocada (html), 
+
+html.classList
+verificando a lista de classes existentes.
+
+Toggle para mudar ao tema oposto!*/
